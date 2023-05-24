c41ebde52a3fba60cbeef048cff65ae376b86cd1
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2023-05-24 20:59:30 +0300

    9.10

--- Patientor-Backend/src/index.ts
+++ Patientor-Backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
-const app = express();
+import diagnoseRouter from './routes/diagnoses';
 import cors from 'cors';
+const app = express();
 app.use(express.json());
 
 const PORT = 3001;
@@ -9,6 +10,8 @@ app.use(cors());
 app.get('/api/ping', (_req, res) => {
     res.send('pong!');
 });
+
+app.use('/api/diagnoses', diagnoseRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
