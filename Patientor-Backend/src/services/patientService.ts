9174e0fc1bb913c9f61c8e5ae3e1030ff4e55eed
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2023-05-25 13:26:51 +0300

    9.11

---
+++ Patientor-Backend/src/services/patientService.ts
@@ -0,0 +1,16 @@
+import patientData from '../../data/patients';
+import { NonSensitivePatient } from '../types';
+
+const getNonSensitivePatients = (): NonSensitivePatient[] => {
+    return patientData.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+        id,
+        name,
+        dateOfBirth,
+        gender,
+        occupation
+    }));
+};
+
+export default {
+    getNonSensitivePatients
+};
