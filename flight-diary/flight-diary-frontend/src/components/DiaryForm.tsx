d21037f40f0e290463de84cd7e59ed7f3d97a2b9
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2023-05-27 19:22:19 +0300

    9.19

--- flight-diary/flight-diary-frontend/src/components/DiaryForm.tsx
+++ flight-diary/flight-diary-frontend/src/components/DiaryForm.tsx
@@ -10,10 +10,6 @@ const DiaryForm = (props: DiaryFormProps) => {
     const createDiary = (event: React.SyntheticEvent) => {
         event.preventDefault()
 
-        if (!visibility || !weather || !date ) {
-            throw new Error('Incorrect or missing input!')
-        }
-
         const diary: NewDiaryEntry = {
             "date": date,
             "visibility": visibility,
@@ -34,11 +30,29 @@ const DiaryForm = (props: DiaryFormProps) => {
             <h2>Add New</h2>
             <form onSubmit={createDiary}>
                 date
-                <input value={date} onChange={(event) => setDate(event.target.value)}/><br/>
-                visibility
-                <input value={visibility} onChange={(event) => setVisibility(event.target.value)}/><br/>
-                weather
-                <input value={weather} onChange={(event) => setWeather(event.target.value)}/><br/>
+                <input type='date' value={date} onChange={(event) => setDate(event.target.value)}/><br/>
+
+                visibility{' '}
+                great
+                <input type='radio' name='visibility' value='great' checked={visibility === 'great'} onChange={(event) => setVisibility(event.target.value)}/>
+                good
+                <input type='radio' name='visibility' value='good' checked={visibility === 'good'} onChange={(event) => setVisibility(event.target.value)}/>
+                ok
+                <input type='radio' name='visibility' value='ok' checked={visibility === 'ok'} onChange={(event) => setVisibility(event.target.value)}/>
+                poor
+                <input type='radio' name='visibility' value='poor' checked={visibility === 'poor'} onChange={(event) => setVisibility(event.target.value)}/><br/>
+
+
+                weather{' '}
+                sunny
+                <input type='radio' name='weather' value='sunny' checked={weather === 'sunny'} onChange={(event) => setWeather(event.target.value)}/>
+                rainy
+                <input type='radio' name='weather' value='rainy' checked={weather === 'rainy'} onChange={(event) => setWeather(event.target.value)}/>
+                cloudy
+                <input type='radio' name='weather' value='cloudy' checked={weather === 'cloudy'} onChange={(event) => setWeather(event.target.value)}/>
+                windy
+                <input type='radio' name='weather' value='windy' checked={weather === 'windy'} onChange={(event) => setWeather(event.target.value)}/><br/>
+
                 comment
                 <input value={comment} onChange={(event) => setComment(event.target.value)}/><br/>
                 <button type='submit'>add</button>
